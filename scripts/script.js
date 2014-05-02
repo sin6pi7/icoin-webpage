296461df28c252968ed6098707814be5b02a4a64
sin6pi7 <jac.zyla@gmail.com>
2014-05-03 01:29:38 +0200

    Added git branch

--- scripts/script.js
+++ scripts/script.js
@@ -11,5 +11,17 @@ jQuery(function($) {
             itemClass: 'nav-item',
             itemHover: 'active'
         });
+        $(".logo-big").load(function (){
+            $('.center-auto').each(function(){
+                var parent_height = $(this).parent().height();
+                var top = (parent_height - $(this).height()) / 2;
+                console.log(parent_height);
+                console.log($(this).height());
+                if(top > 0){
+                    $(this).css('position', 'relative');
+                    $(this).css('top', top);
+                }
+            });
+        });
     });
 });
