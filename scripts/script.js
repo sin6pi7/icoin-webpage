3ba9544f462566e34583e0183f10f2f2bb8536b6
sin6pi7 <jac.zyla@gmail.com>
2014-05-02 14:52:06 +0200

    Finished navbar framework

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+jQuery(function($) {
+    $(document).ready(function () {
+        $('header').stickUp({
+            parts: {
+                0: 'sekcja1',
+                1: 'sekcja2',
+                2: 'sekcja3',
+                3: 'sekcja4',
+                4: 'sekcja5'
+            },
+            itemClass: 'nav-item',
+            itemHover: 'active'
+        });
+    });
+});
