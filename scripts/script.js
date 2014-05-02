f9e9128509ccb3e50a8224685bcf084e1fe160c9
sin6pi7 <jac.zyla@gmail.com>
2014-05-02 21:07:14 +0200

    Up to date files

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ jQuery(function($) {
     $(document).ready(function () {
         $('header').stickUp({
             parts: {
-                0: 'sekcja1',
+                0: 'home',
                 1: 'sekcja2',
                 2: 'sekcja3',
                 3: 'sekcja4',
