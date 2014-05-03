980af09039167ec83e04ab56e6d2e9e1c235ac0e
sin6pi7 <jac.zyla@gmail.com>
2014-05-03 23:49:36 +0200

    Final commit before merge into master

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,10 @@ jQuery(function($) {
         $('header').stickUp({
             parts: {
                 0: 'home',
-                1: 'sekcja2',
-                2: 'sekcja3',
-                3: 'sekcja4',
-                4: 'sekcja5'
+                1: 'our-activity',
+                2: 'study-groups',
+                3: 'interesting-people',
+                4: 'contact'
             },
             itemClass: 'nav-item',
             itemHover: 'active'
@@ -14,9 +14,7 @@ jQuery(function($) {
         $(".logo-big").load(function (){
             $('.center-auto').each(function(){
                 var parent_height = $(this).parent().height();
-                var top = (parent_height - $(this).height()) / 2;
-                console.log(parent_height);
-                console.log($(this).height());
+                var top = (parent_height - $(this).outerHeight(true)) / 2;
                 if(top > 0){
                     $(this).css('position', 'relative');
                     $(this).css('top', top);
