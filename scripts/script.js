1b30a71d5e84b2d8477527789a4fa20f87bd296f
sin6pi7 <jac.zyla@gmail.com>
2014-05-04 02:04:47 +0200

    Clicking an anchor scrolls the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 jQuery(function($) {
     $(document).ready(function () {
+
         $('header').stickUp({
             parts: {
                 0: 'home',
@@ -11,7 +12,8 @@ jQuery(function($) {
             itemClass: 'nav-item',
             itemHover: 'active'
         });
-        $(".logo-big").load(function (){
+
+        $('.logo-big').load(function (){
             $('.center-auto').each(function(){
                 var parent_height = $(this).parent().height();
                 var top = (parent_height - $(this).outerHeight(true)) / 2;
@@ -21,5 +23,27 @@ jQuery(function($) {
                 }
             });
         });
+
+        $('.subsection-anchor').on('click', function (e) {
+            e.preventDefault();
+            var subsectionIndex = ($(this).parent().index() + 1)/2;
+            console.log(subsectionIndex);
+            var subsection = $(this).closest('.main-section').siblings('.subsection')[subsectionIndex-1];
+            $(subsection).scrollTo();
+        });
+
+        $('.nav-item').on('click', function (e) {
+            e.preventDefault();
+            $($(this).attr('href')).scrollTo();
+        })
     });
+
+    (function($) {
+        $.fn.scrollTo = function() {
+            $('html, body').animate({
+                scrollTop: $(this).offset().top + 'px'
+            }, 'fast');
+            return this;
+        }
+    })(jQuery);
 });
