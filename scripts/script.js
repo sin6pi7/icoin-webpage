85d11a5754cbef9f7439d7c0c6d594877a9f8723
sin6pi7 <jac.zyla@gmail.com>
2014-06-08 14:14:18 +0200

    Reduced navbar's header height to 40px by customizing bootstrap source

--- scripts/script.js
+++ scripts/script.js
@@ -7,33 +7,33 @@ $(document).ready(function () {
 });
 
 function init() {
-    $('.logo-big').load(function (){
-        $('.center-auto').each(function(){
+    $(".logo-big").load(function (){
+        $(".center-auto").each(function(){
             var parent_height = $(this).parent().height();
             var top = (parent_height - $(this).outerHeight(true)) / 2;
             if(top > 0){
-                $(this).css('position', 'relative');
-                $(this).css('top', top);
+                $(this).css("position", "relative");
+                $(this).css("top", top);
             }
         });
     });
 
-    $('.subsection-anchor').on('click', function (e) {
+    $(".subsection-anchor").on("click", function (e) {
         e.preventDefault();
         var subsectionIndex = ($(this).parent().index() + 1)/2;
-        var subsection = $(this).closest('.main-section').siblings('.subsection')[subsectionIndex-1];
+        var subsection = $(this).closest(".main-section").siblings(".subsection")[subsectionIndex-1];
         $(subsection).scrollTo();
     });
 
-    $(".nav-item, .logo-anchor").on('click', function (e) {
+    $(".nav-item, .logo-anchor").on("click", function (e) {
         e.preventDefault();
-        $($(this).attr('href')).scrollTo();
+        $($(this).attr("href")).scrollTo();
     });
 
-    $('.navbar').affix({
+    $(".navbar").affix({
         offset: {
             top: function () {
-                return (this.top = ($(window).height() - $('.navbar').height()));
+                return (this.top = ($(window).height() - $(".navbar").height()));
             }
         }
     })
