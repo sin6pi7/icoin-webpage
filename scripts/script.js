89361b3942d2b455cf16ab206bc74ae623579d0f
Jacek Żyła <jac.zyla@gmail.com>
2017-12-31 00:19:39 +0100

    Load sections lazily.

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,130 @@
-$(window).load(function () {
-    init();
-});
-function init() {
-    centerSectionsContent();
-    setUpAnchors();
-    setUpNavbarAffix();
-}
-function centerSectionsContent() {
-    $(".center-auto").each(function(){
-        var parentHeight = $(this).parent().height();
-        var top = (parentHeight - $(this).outerHeight(true)) / 2;
-        if ($(this).hasClass("mission-container")) {
-            top -= $(".icons-and-logo-container").height();
-        }
-        if (top > 0){
-            $(this).css("position", "relative");
-            $(this).css("top", top);
-        }
-    });
-}
-function setUpAnchors() {
+$(window).load(function init() {
+  function setUpAnchors() {
     var href;
-    $(".js-anchor-scroll").on("click", function (e) {
-        href = $(this).attr("href") || $(this).children("a").attr("href");
-        e.preventDefault();
-        $(href).scrollTo();
+    $(".js-anchor-scroll").on("click", function(e) {
+      href =
+        $(this).attr("href") ||
+        $(this)
+          .children("a")
+          .attr("href");
+      e.preventDefault();
+      $(href).scrollTo();
     });
-}
-function setUpNavbarAffix() {
+  }
+  function setUpNavbarAffix() {
     var homepageHeight = $("#home").height(),
-        windowHeight = $(window).height()
-        navbar = $(".navbar");
-        topOffset = homepageHeight - navbar.height();
+      windowHeight = $(window).height();
+    navbar = $(".navbar");
+    topOffset = homepageHeight - navbar.height();
     if (homepageHeight > windowHeight) {
-        topOffset = 0;
+      topOffset = 0;
     }
-    navbar.affix({
+    navbar
+      .affix({
         offset: {
-            top: function () {
-                return (this.top = topOffset);
-            }
+          top: function() {
+            return (this.top = topOffset);
+          }
         }
-    }).on('affix.bs.affix affix-top.bs.affix', function () {
+      })
+      .on("affix.bs.affix affix-top.bs.affix", function() {
         $("#navbar-logo").toggle("slow");
+      });
+  }
+  function setUpSectionsLoader(sections) {
+    var loading = [];
+    var loaded = [];
+    function loadSection(section) {
+      var $container = $(section.containerSelector);
+      if (
+        loading.indexOf(section.path) !== -1 ||
+        loaded.indexOf(section.path) !== -1
+      ) {
+        return;
+      }
+      loading.push(section.path);
+      $.get(section.path)
+        .done(function(html) {
+          loaded.push(section.path);
+          $container.html(html);
+          var $centerAuto = $container.find(".center-auto");
+          var parentHeight = $centerAuto.parent().height();
+          var top = (parentHeight - $centerAuto.outerHeight(true)) / 2;
+          if ($centerAuto.hasClass("mission-container")) {
+            top -= $(".icons-and-logo-container").height();
+          }
+          if (top > 0) {
+            $centerAuto.css("position", "relative");
+            $centerAuto.css("top", top);
+          }
+        })
+        .always(function() {
+          loading.splice(loading.indexOf(section.path), 1);
+        });
+    }
+    sections.forEach(function(section, idx) {
+      var $container = $(section.containerSelector);
+      $container.appear();
+      if ($container.is(":appeared")) {
+        loadSection(section);
+        if (idx > 0) {
+          loadSection(sections[idx - 1]);
+        }
+        if (idx < sections.length - 1) {
+          loadSection(sections[idx + 1]);
+        }
+        return;
+      }
+      $container.on("appear", function() {
+        loadSection(section);
+        if (idx > 0) {
+          loadSection(sections[idx - 1]);
+        }
+        if (idx < sections.length - 1) {
+          loadSection(sections[idx + 1]);
+        }
+      });
     });
-}
+  }
+
+  setUpSectionsLoader([
+    {
+      containerSelector: "#home",
+      path: "/sections/home.html"
+    },
+    {
+      containerSelector: "#financial-abc",
+      path: "/sections/financial-abc.html"
+    },
+    {
+      containerSelector: "#financial-abc-subsection1",
+      path: "/sections/financial-abc-subsection1.html"
+    },
+    {
+      containerSelector: "#financial-abc-subsection2",
+      path: "/sections/financial-abc-subsection2.html"
+    },
+    {
+      containerSelector: "#financial-abc-subsection3",
+      path: "/sections/financial-abc-subsection3.html"
+    },
+    {
+      containerSelector: "#experts",
+      path: "/sections/experts.html"
+    },
+    {
+      containerSelector: "#experts-subsection1",
+      path: "/sections/experts-subsection1.html"
+    },
+    {
+      containerSelector: "#cofounder",
+      path: "/sections/cofounder.html"
+    },
+    {
+      containerSelector: "#contact",
+      path: "/sections/contact.html"
+    }
+  ]);
+  setUpAnchors();
+  setUpNavbarAffix();
+});
